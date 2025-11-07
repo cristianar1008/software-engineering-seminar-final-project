1162c9d8be766da0cdc66a00ec1ee5854d5a839b
Filomaticom <192216517+FiloFromZero@users.noreply.github.com>
2025-11-07 16:36:42 -0500

    Vista de Login
    Se agrega vista de Login en Angular

--- Final Project/Workshop 3/front-end/src/app/app.ts
+++ Final Project/Workshop 3/front-end/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,10 +9,15 @@ import { FooterComponent } from './layout/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('secretary-portal');
+  constructor(private router: Router) {}
+
+  isLoginRoute(): boolean {
+    return this.router.url.startsWith('/login');
+  }
 }

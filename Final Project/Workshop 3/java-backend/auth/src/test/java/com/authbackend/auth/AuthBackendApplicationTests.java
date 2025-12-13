5ee4f673b80baa753bfb8e4b1531b7962a728f4c
Filomaticom <192216517+FiloFromZero@users.noreply.github.com>
2025-12-12 20:02:18 -0500

    Fix CI/CD

--- Final Project/Workshop 3/java-backend/auth/src/test/java/com/authbackend/auth/AuthBackendApplicationTests.java
+++ Final Project/Workshop 3/java-backend/auth/src/test/java/com/authbackend/auth/AuthBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.authbackend.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthBackendApplicationTests {
 
 	@Test
